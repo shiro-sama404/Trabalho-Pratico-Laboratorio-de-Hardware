88b8a1051d8e790fd6fd196a4149f292f7e5f31c
Arthur Barbosa <arthurbarbosaandrade2003@gmail.com>
2024-10-27 16:27:06 -0400

    Atualização das testbenchs
    Testbenchs das entidades trava e binto7seg atualziadas

--- codigo/tb_binto7seg.vhd
+++ codigo/tb_binto7seg.vhd
@@ -8,13 +8,49 @@ architecture testbench of tb_binto7seg is
     signal input: in std_logic_vector(3 downto 0);    -- Valor binario a ser mostrado
     signal display: out std_logic_vector(7 downto 0); -- Leds do Display de 7 seg.
 
+    -- Matriz com os valores esperados para cada entrada de 0 a 15
+    type display_array is array (0 to 15) of std_logic_vector(7 downto 0);
+    constant expected_display: display_array := (
+        "11111100",  -- 0
+        "01100000",  -- 1
+        "11011010",  -- 2
+        "11110010",  -- 3
+        "01100110",  -- 4
+        "10110110",  -- 5
+        "10111110",  -- 6
+        "11100000",  -- 7
+        "11111100",  -- 8
+        "11100110",  -- 9
+        "11101110",  -- A
+        "00111110",  -- b
+        "10011100",  -- C
+        "01111010",  -- d
+        "10011110",  -- E
+        "10001110"   -- F
+    );
+
 begin
 
-    lock: entity work.binto7seg
+    -- Instância da entidade binto7seg
+    seg7: entity work.binto7seg
         port map (input, display);
 
+    -- Processo de estímulos
     stim_proc: process
     begin
+        - Testar todos os valores de 0 a 15
+        for i in 0 to 15 loop
+
+            input <= std_logic_vector(to_unsigned(i, 4));
+            wait for 10 ns;
+
+            -- Verifica se o valor do display está correto
+            assert display = expected_display(i) report "Erro: " & integer'image(i) & " não mapeado corretamente!" severity error;
+
+        end loop;
+
         wait;
+
     end process;
+
 end architecture;
--- codigo/tb_trava.vhd
+++ codigo/tb_trava.vhd
@@ -5,8 +5,9 @@ entity tb_trava is
 end entity;
 
 architecture testbench of tb_trava is
-    signal senha: natural range 0 to 255; -- Número usado como senha para destravar
-    signal tempo_para_desarme: natural range 0 to 255 -- Em segundos
+
+    signal senha: natural range 0 to 255 := 84; -- Número usado como senha para destravar
+    signal tempo_para_desarme: natural range 0 to 255 := 10 -- Em segundos
     signal clock: std_logic; -- Entrada de clock 1hz para contagem do tempo
     signal reset: std_logic; -- Reset do tempo
     signal input: std_logic_vector(7 downto 0); -- Chaves para destravar
@@ -19,8 +20,49 @@ begin
         generic map (senha, tempo_para_desarme)
         port map (clock, reset, input, segundos, trava);
 
-    stim_proc: process
+    -- Geração do clock
+    clk_process : process
     begin
+        -- Ciclo de clock de 1 segundo
+        while true loop
+            clock <= '1';
+            wait for 0.5s;  
+            clock <= '0';
+            wait for 0.5s;
+        end loop;
+    end process;
+
+    -- Processo de estímulos
+    stim_proc : process
+    begin
+        -- Início com reset
+        reset <= '1';
+        wait for 1s;
+        reset <= '0';
+        wait for 1s;
+
+        -- Tenta desbloquear com senha errada dentro do tempo de desarme
+        input <= std_logic_vector(to_unsigned(111, 8));
+        wait for 10ns;
+        assert trava = '1' report message "Trava desbloquada com senha errada dentro do tempo de desarme" severity error;
+        wait for 9s;
+
+        -- Tentar desbloquear com senha certa fora do tempo de desarme
+        input <= std_logic_vector(to_unsigned(senha, 8));
+        wait for 10ns;
+        assert trava = '1' report message "Trava desbloquada com senha certa fora do tempo de desarme" severity error;
+
+        -- reset
+        reset <= '1';
+        wait for 1s;
+        reset <= '0';
+        wait for 1s;
+
+        -- Tentar desbloquear com senha certa dentro do tempo de desarme
+        input <= std_logic_vector(to_unsigned(senha, 8));
+        assert trava = '0' report message "Trava bloqueada com senha certa dentro do tempo de desarme" severity error;
+        wait for 2s;
+
         wait;
     end process;
 end architecture;
