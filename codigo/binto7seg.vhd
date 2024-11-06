71f49f15a2825b66def86ca411ad7b8253773a70
Link <88507400+lincoln-lima@users.noreply.github.com>
2024-11-06 11:28:03 -0400

    Update binto7seg.vhd

--- codigo/binto7seg.vhd
+++ codigo/binto7seg.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 entity binto7seg is
     port (
         input  : in std_logic_vector(3 downto 0);  -- Valor binario a ser mostrado
-        display: out std_logic_vector(7 downto 0); -- Leds do Display de 7 seg.
+        display: out std_logic_vector(7 downto 0) -- Leds do Display de 7 seg.
     );
 end entity;
 
