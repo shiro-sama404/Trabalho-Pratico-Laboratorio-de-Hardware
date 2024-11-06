645308089896a9de72c754953b805e031c31355f
Link <88507400+lincoln-lima@users.noreply.github.com>
2024-11-06 11:30:05 -0400

    Update tb_trava.vhd

--- codigo/tb_trava.vhd
+++ codigo/tb_trava.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity tb_trava is
 end entity;
@@ -7,12 +8,12 @@ end entity;
 architecture testbench of tb_trava is
 
     signal senha: natural range 0 to 255 := 84;             -- Número usado como senha para destravar
-    signal tempo_para_desarme: natural range 0 to 255 := 10 -- Em segundos
+    signal tempo_para_desarme: natural range 0 to 255 := 10; -- Em segundos
     signal clock: std_logic;                                -- Entrada de clock 1hz para contagem do tempo
     signal reset: std_logic;                                -- Reset do tempo
     signal input: std_logic_vector(7 downto 0);             -- Chaves para destravar
     signal segundos: std_logic_vector(7 downto 0);          -- Tempo para desbloqueio
-    signal trava: std_logic                                 -- Sinal de led: 1 para travado, 0 para destravado
+    signal trava: std_logic;                                 -- Sinal de led: 1 para travado, 0 para destravado
 
 begin
 
@@ -26,9 +27,9 @@ begin
         -- Ciclo de clock de 1 segundo
         while true loop
             clock <= '1';
-            wait for 0.5s;  
+            wait for 0.5 sec;  
             clock <= '0';
-            wait for 0.5s;
+            wait for 0.5 sec;
         end loop;
     end process;
 
@@ -37,31 +38,31 @@ begin
     begin
         -- Início com reset
         reset <= '1';
-        wait for 1s;
+        wait for 1 sec;
         reset <= '0';
-        wait for 1s;
+        wait for 1 sec;
 
         -- Tenta desbloquear com senha errada dentro do tempo de desarme
         input <= std_logic_vector(to_unsigned(111, 8));
-        wait for 10ns;
-        assert trava = '1' report message "Trava desbloquada com senha errada dentro do tempo de desarme" severity error;
-        wait for 9s;
+        wait for 10 ns;
+        assert trava = '1' report "Trava desbloquada com senha errada dentro do tempo de desarme" severity error;
+        wait for 9 sec;
 
         -- Tentar desbloquear com senha certa fora do tempo de desarme
         input <= std_logic_vector(to_unsigned(senha, 8));
-        wait for 10ns;
-        assert trava = '1' report message "Trava desbloquada com senha certa fora do tempo de desarme" severity error;
+        wait for 10 ns;
+        assert trava = '1' report "Trava desbloquada com senha certa fora do tempo de desarme" severity error;
 
         -- reset
         reset <= '1';
-        wait for 1s;
+        wait for 1 sec;
         reset <= '0';
-        wait for 1s;
+        wait for 1 sec;
 
         -- Tentar desbloquear com senha certa dentro do tempo de desarme
         input <= std_logic_vector(to_unsigned(senha, 8));
-        assert trava = '0' report message "Trava bloqueada com senha certa dentro do tempo de desarme" severity error;
-        wait for 2s;
+        assert trava = '0' report "Trava bloqueada com senha certa dentro do tempo de desarme" severity error;
+        wait for 2 sec;
 
         wait;
     end process;
